4fce7b6fbe3a08a5dee5bde17575cc964816511a
MohamedAzizGharbi <gharbi.aziz03@gmail.com>
2025-12-07 04:24:05 +0100

    day1p2aoc2025

--- Advent_Of_Code/adventOfCode2025/day01/main.cpp
+++ Advent_Of_Code/adventOfCode2025/day01/main.cpp
@@ -3,42 +3,44 @@
 using namespace std;
 
 int main() {
-  // ifstream fin("input.txt");
-  ifstream fin("test.txt");
+  ifstream fin("input.txt");
+  // ifstream fin("test.txt");
   cout << "Day 1 Advent of Code:" << endl;
   string line;
   int ans = 0;
   int cursor = 50;
   char c;
   int diff;
-  string wiw = "wiiiiw";
   while (getline(fin, line)) {
     c = line[0];
     diff = stoi(line.substr(1));
+    // Calculating the full loops
+    ans += (diff / 100);
+    int remaining = diff % 100;
     switch (c) {
     case 'L':
-      ans += diff / 100;
-      diff = diff % 100;
-			if(( cursor-diff ) < 0){
-					ans++;
-			}
-      if (diff > cursor) {
-        cursor = cursor - diff + 100;
-      } else
-        cursor = cursor - diff;
+      if (remaining > cursor) {
+        if (cursor != 0)
+          ans++;
+        cursor = 100 + (cursor - remaining);
+      } else {
+        cursor -= remaining;
+        if (cursor == 0)
+          ans++;
+      }
       break;
     case 'R':
-      ans += diff / 100;
-      cursor = (cursor + diff) % 100;
-      diff = diff % 100;
-      if ((cursor + diff) > 100) {
-        ans++;
+      if ((remaining + cursor) > 99) {
+        if (cursor != 0)
+          ans++;
+        cursor = remaining + cursor - 100;
+      } else {
+        cursor += remaining;
+        if (cursor == 0)
+          ans++;
       }
       break;
     }
-    // cout << "CURR  : " << cursor << "\n";
-    if (cursor == 0)
-      ans++;
   }
   cout << ans << "\n";
   return 0;
